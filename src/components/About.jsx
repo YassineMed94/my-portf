11475e42c0e731777267c02cfc26d02fc1ed7d87
Yassine Medah <yassinemedah1994@gmail.com>
2024-05-15 21:14:17 +0200

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        I am a frontend and fullstack web developer with a passion for crafting immersive digital experiences. 
+        With a strong background in MERN stack, I specialize in Javascript and specialy in React js. My portfolio showcases a range of projects where I've demonstrated 
+        my expertise in creating user-friendly interfaces and robust backend systems. 
+        I thrive on challenges and constantly seek to expand my skill set to stay at the forefront of web development trends.
+         With a commitment to quality and innovation, I aim to deliver impactful solutions that exceed client expectations.
+          Let's connect and bring your ideas to life!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
---
+++ src/components/Education.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Education() {
+  return (
+    <h1 className='color-white'>Education</h1>
+  )
+}
+
+export default Education
